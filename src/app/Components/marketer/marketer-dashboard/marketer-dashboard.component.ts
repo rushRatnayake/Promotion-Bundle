c3dbabe454292a21be3c43c3feda427fa01ba81c
Thamashi Kumarasinghe <kthamashi@gmail.com>
2021-05-01 18:28:35 +0530

    Css changes on main menu

--- src/app/Components/marketer/marketer-dashboard/marketer-dashboard.component.ts
+++ src/app/Components/marketer/marketer-dashboard/marketer-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from 'src/app/Services/dashboard/dashboard.service';
+// import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+// import { Label } from 'ng2-charts';
+// import { ChartType, ChartOptions, ChartDataSets } from 'chart.js';
 
 @Component({
   selector: 'app-marketer-dashboard',
@@ -8,9 +11,25 @@ import { DashboardService } from 'src/app/Services/dashboard/dashboard.service';
 })
 export class MarketerDashboardComponent implements OnInit {
 
-  constructor(
-    private dashboardService: DashboardService
-  ) { }
+  constructor(private dashboardService: DashboardService) {}
+  // For Pie Chart
+  // public pieChartOptions: ChartOptions = {
+  //   responsive: true,
+  //   // legend: {
+  //   //   position: 'top',
+  //   // }
+  // };
+  // public pieChartLabels: Label[] = [];
+  // public pieChartData: number[] = [300, 500, 100];
+  // public pieChartType: ChartType = 'pie';
+  // public pieChartLegend = true;
+  // public pieChartPlugins = [pluginDataLabels];
+  // public pieChartColors = [
+  //   {
+  //     backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)'],
+  //   },
+  // ];
+
 
   ngOnInit(): void {
     this.loadDetails();
